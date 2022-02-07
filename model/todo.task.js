905f7476f070e396bd3f8c131ec83eeacf355c4d
danilverisokin <verisokindaniil@gmail.com>
2022-02-07 11:45:35 +0300

    Install sqlite extension

--- model/todo.task.js
+++ model/todo.task.js
@@ -1,5 +1,5 @@
-const { Sequrlize, DataTypes } = require("sequelize");
-const sequelize = new Sequrlize("splite::memory:");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
 
 const Task = sequelize.define("Task", {
   uuid: {
@@ -16,3 +16,9 @@ const Task = sequelize.define("Task", {
     type: DataTypes.DATE,
   },
 });
+
+(async () => {
+  await sequelize.sync({ force: true });
+})();
+
+module.exports = Task;
